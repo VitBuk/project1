588fdb6b321dfe9e6135279f9fa282b45eee6ab3
VitBuk <vitbuk@gmail.com>
2023-02-14 00:49:56 +0200

    new person methods implemented

--- src/main/java/com/vitbuk/spring/controllers/PeopleController.java
+++ src/main/java/com/vitbuk/spring/controllers/PeopleController.java
@@ -1,11 +1,17 @@
 package com.vitbuk.spring.controllers;
 
 import com.vitbuk.spring.dao.PersonDAO;
+import com.vitbuk.spring.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -22,5 +28,23 @@ public class PeopleController {
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person perosn) {
+       return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person, BindingResult bindingResult) {
+
+        // в bindingResult складываются и простейшие ошибки с @Valid и сложные с personValidatora
+        //personValidator.validate(person, bindingResult);
+
+       // if(bindingResult.hasErrors())
+       //     return "people/new";
+
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
