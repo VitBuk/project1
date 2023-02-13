dff36673af5257e1e05bd3d902c49003214aa551
VitBuk <vitbuk@gmail.com>
2023-02-13 23:39:16 +0200

    personDAO methods implemented

--- src/main/java/com/vitbuk/spring/dao/PersonDAO.java
+++ src/main/java/com/vitbuk/spring/dao/PersonDAO.java
@@ -1,4 +1,43 @@
 package com.vitbuk.spring.dao;
 
+import com.vitbuk.spring.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Component
 public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person",
+                new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person(name, year_of_birth) VALUES(?,?)",
+                person.getName(), person.getYear_of_birth());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=?, year_of_birth WHERE id=?",
+                updatedPerson.getName(),updatedPerson.getYear_of_birth(), updatedPerson.getId());
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+
 }
