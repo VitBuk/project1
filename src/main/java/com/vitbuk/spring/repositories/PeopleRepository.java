74a96a5fbe2eb128a5d4bfc61de9fc3b2b3d1c6c
VitBuk <vitbuk@gmail.com>
2023-03-11 11:55:24 +0200

    PeopleRepository initialized

--- src/main/java/com/vitbuk/spring/repositories/PeopleRepository.java
+++ src/main/java/com/vitbuk/spring/repositories/PeopleRepository.java
@@ -1,4 +1,9 @@
 package com.vitbuk.spring.repositories;
 
-public interface PeopleRepository {
+import com.vitbuk.spring.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
 }
