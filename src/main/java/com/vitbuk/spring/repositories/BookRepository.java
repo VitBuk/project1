f4bad756af5ee33dacb1381eee525d1cee40ca4d
VitBuk <vitbuk@gmail.com>
2023-03-12 23:59:11 +0200

    BookRepository added

---
+++ src/main/java/com/vitbuk/spring/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.vitbuk.spring.repositories;
+
+import com.vitbuk.spring.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
