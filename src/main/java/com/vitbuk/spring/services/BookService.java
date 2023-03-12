5701f96dc7e16fa7344c3a72d31722fa75ec2907
VitBuk <vitbuk@gmail.com>
2023-03-13 00:00:19 +0200

    BookService initialized

--- src/main/java/com/vitbuk/spring/services/BookService.java
+++ src/main/java/com/vitbuk/spring/services/BookService.java
@@ -1,4 +1,18 @@
 package com.vitbuk.spring.services;
 
+import com.vitbuk.spring.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 }
