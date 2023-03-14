09b67dc3055ac2e26ac1e1270c380305b835edaa
VitBuk <vitbuk@gmail.com>
2023-03-14 09:54:04 +0200

    method show in PeopleController now add list of books to model attributes

--- src/main/java/com/vitbuk/spring/controllers/PeopleController.java
+++ src/main/java/com/vitbuk/spring/controllers/PeopleController.java
@@ -41,6 +41,7 @@ public class PeopleController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", peopleService.findOne(id));
+        model.addAttribute("books", peopleService.getBooksByPersonId(id));
         return "people/show";
     }
 
