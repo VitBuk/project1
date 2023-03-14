0a4f8a6eeffa3381892c74238a18e8d2e7a28fad
VitBuk <vitbuk@gmail.com>
2023-03-14 09:25:39 +0200

    annotations added to Person model

--- src/main/java/com/vitbuk/spring/models/Person.java
+++ src/main/java/com/vitbuk/spring/models/Person.java
@@ -1,31 +1,40 @@
 package com.vitbuk.spring.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.List;
 
+@Entity
+@Table(name = "Person")
 public class Person {
 
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
+    @Size(min=1, max=100, message = "Name should be from 1 to 100 symbols")
     private String name;
 
     @NotEmpty(message = "year of birth should not be empty")
     @Pattern(regexp = "\\d{4}", message = "year should have 4 digits")
+    @Column(name="year_of_birth")
+    @Min(value=1900, message = "Year of birth should be more than 1900")
     private int year_of_birth;
 
+    @OneToMany(mappedBy = "owner")
     private List<Book> books;
 
     public Person() {
     }
 
-    public Person(int id, String name, int year_of_birth, List<Book> books) {
-        this.id = id;
+    public Person(String name, int year_of_birth) {
         this.name = name;
         this.year_of_birth = year_of_birth;
-        this.books = books;
     }
 
     public int getId() {
@@ -59,4 +68,5 @@ public class Person {
     public void setBooks(List<Book> books) {
         this.books = books;
     }
+
 }
