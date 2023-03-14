c9d4d0c563f1eeb4e4bf8b708ab8e887420a2d96
VitBuk <vitbuk@gmail.com>
2023-03-14 11:48:41 +0200

    all from PersonDAO and BookDAO deleted. Thos methods only need for specific SQL querries

--- src/main/java/com/vitbuk/spring/dao/PersonDAO.java
+++ src/main/java/com/vitbuk/spring/dao/PersonDAO.java
@@ -10,44 +10,4 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class PersonDAO {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person",
-                new BeanPropertyRowMapper<>(Person.class));
-    }
-
-    public Person show(int id) {
-        Person person = jdbcTemplate.query("SELECT * FROM Person WHERE id=?",
-                new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
-
-        person.setBooks(booksOfPerson(id));
-        return person;
-    }
-
-    public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(name, year_of_birth) VALUES(?,?)",
-                person.getName(), person.getYear_of_birth());
-    }
-
-    public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("UPDATE Person SET name=?, year_of_birth=? WHERE id=?",
-                updatedPerson.getName(),updatedPerson.getYear_of_birth(), updatedPerson.getId());
-    }
-
-    public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
-    }
-
-    public List<Book> booksOfPerson(int id) {
-        String sql = "SELECT * FROM BOOK WHERE person_id=" + id;
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Book.class));
-    }
-}
+public class PersonDAO {}
